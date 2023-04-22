af08eb1230ad081faa58134bcec334a27631a24d
Gabriel Henrique <gabrieltjs75@gmail.com>
2023-04-21 21:51:29 -0300

    Tela de Regsitro de contato

--- config/process.php
+++ config/process.php
@@ -1,13 +1,23 @@
 <?php
 
 session_start();
-include_once ("connection.php");
-include_once ("url.php");
+include_once "connection.php";
+include_once "url.php";
 
-$contacts = [];
+$id;
 
-$query = "SELECT * FROM contacts";
-$stmt = $conn->prepare($query);
-
-$stmt->execute();
-$contacts = $stmt->fetchAll();
+if (!empty($_GET)) {
+    $id = $_GET["id"];
+    $query = "SELECT * FROM contacts WHERE id = :id";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+    $contact = $stmt->fetch();
+} else {
+    // Retorna todos os contatos
+    $contacts = [];
+    $query = "SELECT * FROM contacts";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $contacts = $stmt->fetchAll();
+}
